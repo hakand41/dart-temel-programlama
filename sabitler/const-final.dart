1ab72d0b24f79ecefaee4525493136f43b25d589
Hakan Değer <98481026+hakand41@users.noreply.github.com>
2024-01-31 11:40:07 +0300

    sabitler tekrar

--- sabitler/const-final.dart
+++ sabitler/const-final.dart
@@ -10,4 +10,7 @@ void main() {
   final int c = 1;
   // c'nin değeri değişmez --> c = 2;
   print(c);
+
+  // final ve const arasındaki fark: final çalışma zamanında değer alır, const derleme zamanında.
+  // Ayrıca final değişkenlerin değeri derleme zamanında bilinmeli, const değişkenlerin değeri derleme zamanında bilinmeli ve sabit olmalı.
 }
