9f515af2b225e647befc934e2de418ad1974714b
Hakan Değer <98481026+hakand41@users.noreply.github.com>
2024-01-31 11:09:24 +0300

    operatorler

---
+++ operatorler/operatorler.dart
@@ -0,0 +1,38 @@
+void main() {
+  // = atama operatoru
+  // > büyüktür operatoru
+  // == eşit midir operatoru
+  // != eşit değil midir operatoru
+  // < küçüktür operatoru
+  // >= büyük eşit midir operatoru
+  // <= küçük eşit midir operatoru
+  // && ve operatoru
+  // || veya operatoru
+  // ! değil midir operatoru
+  // ?? null ise operatoru
+  // ??= null ise atama operatoru
+  // + toplama operatoru
+  // - çıkarma operatoru
+  // * çarpma operatoru
+  // / bölme operatoru
+  // ~/ bölüm operatoru
+  // % mod operatoru
+  // ++ arttırma operatoru
+  // -- azaltma operatoru
+  // += toplama atama operatoru
+  // -= çıkarma atama operatoru
+  // *= çarpma atama operatoru
+  // /= bölme atama operatoru
+  // ~/= bölüm atama operatoru
+  // %= mod atama operatoru
+  // [] index operatoru
+  // . nokta operatoru
+  // ( ) parantez operatoru
+  // { } süslü parantez operatoru
+  // [ ] köşeli parantez operatoru
+  // < > açık kapalı parantez operatoru
+  // ; noktalı virgül operatoru
+  // : iki nokta operatoru
+  // .. kaskad operatoru
+  // => ok işareti operatoru
+}
